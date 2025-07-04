aac3509dc3721ca94e9462a9739336d257ced4a3
Jill Fiore <contact@lumaeris.com>
2025-07-04 18:46:24 +0300

    init
    just trying out at least

---
+++ files/scripts/opengamepad-ui-session.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+set -oue pipefail
+
+pushd . > /dev/null
+dir="$(mktemp -d)"
+cd "${dir}"
+wget https://github.com/ShadowBlip/gamescope-session-opengamepadui/archive/refs/heads/main.zip
+unzip main.zip
+cp -R ./gamescope-session-opengamepadui-main/usr/share/* /usr/share/
+popd > /dev/null
+rm -rf "${dir}"
+rm /usr/share/wayland-sessions/gamescope-session.desktop
+ln -s gamescope-session-opengamepadui.desktop /usr/share/wayland-sessions/gamescope-session.desktop
